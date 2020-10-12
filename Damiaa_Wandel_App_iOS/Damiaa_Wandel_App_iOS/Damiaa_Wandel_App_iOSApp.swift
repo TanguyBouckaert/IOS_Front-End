0eb91d68e18aa25688309f2c1ac5c97d864ebd88
Tanguy Bouckaert <tanguy.bouckaert@student.hogent.com>
2020-10-12 19:17:40 +0200

    Starter files

---
+++ Damiaa_Wandel_App_iOS/Damiaa_Wandel_App_iOS/Damiaa_Wandel_App_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Damiaa_Wandel_App_iOSApp.swift
+//  Damiaa_Wandel_App_iOS
+//
+//  Created by Tanguy Bouckaert on 12/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Damiaa_Wandel_App_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
